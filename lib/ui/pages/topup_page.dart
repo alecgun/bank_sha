f08f26ad7e805067f0abdd70affcd6b062b58097
alecgun <alecgun@student.ub.ac.id>
2023-02-13 02:14:30 +0700

    top up page: add wallet and bank list section

--- lib/ui/pages/topup_page.dart
+++ lib/ui/pages/topup_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/widgets/bank_list.dart';
+import 'package:bank_sha/widgets/buttons.dart';
 import 'package:flutter/material.dart';
 
 class TopUpPage extends StatelessWidget {
@@ -23,7 +25,94 @@ class TopUpPage extends StatelessWidget {
         ),
       ),
       body: ListView(
-        children: [],
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        children: [
+          walletSection(),
+          bankSection(),
+          SizedBox(
+            height: 30,
+          ),
+          CustomFilledButton(
+            title: 'Continue',
+            width: double.infinity,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget walletSection() {
+    return Container(
+      margin: EdgeInsets.only(top: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Wallet',
+            style: blackTS.copyWith(
+              fontSize: 16,
+              fontWeight: semibold,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Image.asset(
+                'assets/images/ic_wallet.png',
+                width: 80,
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '8008 2208 1996',
+                    style: blackTS.copyWith(
+                      fontWeight: semibold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    'Shayna Hanna',
+                    style: greyTS.copyWith(
+                      fontSize: 12,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget bankSection() {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Select Bank',
+            style: blackTS.copyWith(
+              fontWeight: semibold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 9,
+          ),
+          BankList(bankName: 'BANK BCA', iconUrl: 'assets/images/ic_bca.png'),
+          BankList(bankName: 'BANK BCA', iconUrl: 'assets/images/ic_bca.png'),
+          BankList(bankName: 'BANK BCA', iconUrl: 'assets/images/ic_bca.png'),
+          BankList(bankName: 'BANK BCA', iconUrl: 'assets/images/ic_bca.png'),
+        ],
       ),
     );
   }
